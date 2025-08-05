011162c807d2597bf38e2c1bb7a093b75b91c9aa
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-08-05 14:57:38 +0530

    classend

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const modal = document.getElementById("modal")
 const modalCloseBtn = document.getElementById("modal-close-btn")
+const consentForm = document.getElementById("consent-form")
+const modalText = document.getElementById("modal-text")
+
+
 
 function timer(){
 modal.style.display = "block"
@@ -11,6 +15,24 @@ setTimeout(timer,1500)
 modalCloseBtn.addEventListener("click", function(){
 modal.style.display = "none"
 })
+
+consentForm.addEventListener("submit" ,function(e){
+     e.preventDefault();
+    modalText.innerHTML = `<div class="modal-inner-loading">
+			<img src="images/loading.svg" class="loading">
+			<p id="uploadText">Uploading your data to the dark web...</p>
+		</div>`
+
+        setTimeout(function() {
+            document.getElementById("uploadText").textContent="Making the sale...";
+
+        }, 1500);
+})
+
+
+
+
+
 
 // console.log("1")
 
