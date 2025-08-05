f4430496a220e936fee7f97bc376b7c01040471d
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-08-05 13:46:55 +0530

    ch

--- forms/index.js
+++ forms/index.js
@@ -1,3 +1,14 @@
+const loginForm = document.getElementById("login-form")
+console.log("from refreshed the js")
 
+loginForm.addEventListener("submit", function(e){
+    e.preventDefault();
 
+    console.log(e)
+    console.log(e.target)
+    console,log(e.target,id)
 
+    const loginFormData = new FormData(loginForm)
+    console.log(loginFormData)
+
+})
--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-console.log("1")
+const modal = document.getElementById("modal")
+const modalCloseBtn = document.getElementById("modal-close-btn")
 
-function runner(){
-console.log("2")
+function timer(){
+modal.style.display = "block"
 }
-setTimeout(runner,900)
 
-console.log("3")
+setTimeout(timer,1500)
+
+
+modalCloseBtn.addEventListener("click", function(){
+modal.style.display = "none"
+})
+
+// console.log("1")
+
+// function runner(){
+// console.log("2")
+// }
+// setTimeout(runner,900)
+
+// console.log("3")
+
+
+// function first(){
+//     console.log("first")
+// }
+
+// function second(){
+//     console.log("second")
+// }
+
+// function third(){
+//     console.log("third")
+// }
+
+// setTimeout(first,1500)
+// setTimeout(second,2500)
+// setTimeout(third,3500)
